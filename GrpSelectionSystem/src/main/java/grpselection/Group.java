b153c2372849301b5ec55b6e2f7f565093c07de2
Siyathemba Msimang <siyathembamsimang@gmail.com>
2026-04-01 09:35:23 +0200

    Update Group.java

--- GrpSelectionSystem/src/main/java/grpselection/Group.java
+++ GrpSelectionSystem/src/main/java/grpselection/Group.java
@@ -1,58 +1,54 @@
 package grpselection;
 
 import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-public class Group{
-	private String groupName;
-	private String leaderName;
-	private int maxMembers;
-	private ArrayList<Participant> members;
-	
-	public Group(String groupName, String leaderName, int maxMembers) {
-		this.groupName = groupName;
-		this.leaderName = leaderName;
-		this.maxMembers = maxMembers;
-		this.members = new ArrayList<>();
-	}
-	public Group() {}
-	//Getters
-	public String getGrpName() {
-		return groupName;
-	}
-	public String getLeaderName() {
-		return leaderName;
-	}
-	public int getMemberCount() {
-		return maxMembers;
-	}
-	//Core methods
-	public boolean isFull() {
-		return members.size() >= maxMembers;
-	}
-	public void addMember(Participant participant) {
-		if (!isFull()) {
-			members.add(participant);
-			participant.setGroup(this); // Assign participant to this group
-		}
-	}
-	//Utility method
-	public void printMembers() {
-		System.out.println("Members of " + groupName + ":");
-		for (Participant p : members) {
-			System.out.println("- " + p.getName());
-		}
-	}
-	public ArrayList<Participant> getMembers() {
-		return members;
-	}
-	public void setMaxMembers(int maxMembers) {
-	    this.maxMembers = maxMembers;
-	}
-	public void setGrpName(String groupName) {
-	    this.groupName = groupName;
-	}
+@JsonIgnoreProperties({"group"})
+public class Group {
 
-	public void setLeaderName(String leaderName) {
-	    this.leaderName = leaderName;
-	}
+    @JsonProperty("groupName")
+    private String groupName;
+
+    @JsonProperty("leaderName")
+    private String leaderName;
+
+    @JsonProperty("maxMembers")
+    private int maxMembers;
+
+    @JsonProperty("members")
+    private ArrayList<Participant> members;
+
+    public Group() { this.members = new ArrayList<>(); }
+
+    public Group(String groupName, String leaderName, int maxMembers) {
+        this.groupName = groupName;
+        this.leaderName = leaderName;
+        this.maxMembers = maxMembers;
+        this.members = new ArrayList<>();
+    }
+
+    public String getGroupName() { return groupName; }
+    public String getLeaderName() { return leaderName; }
+    public int getMaxMembers() { return maxMembers; }
+    public ArrayList<Participant> getMembers() { return members; }
+    public void setGroupName(String groupName) { this.groupName = groupName; }
+    public void setLeaderName(String leaderName) { this.leaderName = leaderName; }
+    public void setMaxMembers(int maxMembers) { this.maxMembers = maxMembers; }
+
+    public boolean isFull() { return members.size() >= maxMembers; }
+
+    public void addMember(Participant participant) {
+        if (!isFull()) {
+            members.add(participant);
+            participant.setGroup(this);
+        }
+    }
+
+    public void printMembers() {
+        System.out.println("Members of " + groupName + ":");
+        for (Participant p : members) {
+            System.out.println("- " + p.getName());
+        }
+    }
 }
